fb1f876fae09062955299620cad7f16f4fe01f36
rendy.harimayco <rendy.harimayco@aia.com>
2023-05-19 09:48:34 +0700

    add basepath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  basePath: '/PDFActions',
   pwa: {
     dest: "public",
     runtimeCaching,
